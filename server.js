2b782a5211f1b7b5b63b69b43c183d71c7f6a1db
jenndroid <jennifer.sesaybarnes@gmail.com>
2020-08-27 11:30:47 +0100

    Writes signin function

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 require('dotenv').config();
 const PORT = process.env.PORT || 4000;
-
-//bring in handlers with format const nameOfHandlers = require('./middleware/file')
+//bring in handlers with format const nameOfHandlers = require('./handlers/file')
+const users = require('./handlers/users');
+const harvests = require('./handlers/harvests');
 
 //bring in middleware with format const nameOfMiddleware = require('./middleware/file')
 const handleErrors = require('./middleware/errorHandling');
@@ -11,5 +12,11 @@ const server = express();
 server.use(express.json());
 
 server.use(handleErrors);
+
+//route to create a user
+server.post('/signin', users.signin);
+
+// route to login
+// server.post('/login', users.login);
 
 server.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
